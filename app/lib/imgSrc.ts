3f974c5cdeb841f83015f1bed10e6192eba7d0d8
Mars SKinner <marsskinner@pop-os.taileb5afe.ts.net>
2026-06-17 19:21:18 -0400

    Fix imgSrc to always include basePath

--- app/lib/imgSrc.ts
+++ app/lib/imgSrc.ts
@@ -1,5 +1,6 @@
 export function imgSrc(src: string): string {
   const base = process.env.__NEXT_ROUTER_BASEPATH || "/steve-trees-website";
-  if (src.startsWith("http") || src.startsWith(base)) return src;
-  return `${base}${src}`;
+  if (src.startsWith("http")) return src;
+  const path = src.startsWith("/") ? src : `/${src}`;
+  return `${base}${path}`;
 }
